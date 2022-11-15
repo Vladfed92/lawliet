2ab0310192ce99eeca5786af4aeb6a20faf25160
Vladislav Fedorenko <107477959+Vladfed92@users.noreply.github.com>
2022-11-15 10:28:33 +0200

    save result before making splite slider

--- src/js/app.js
+++ src/js/app.js
@@ -9,12 +9,8 @@ $(document).ready(function () {
 	});
 });
 
-	// SPLIDE SLIDER //
-
-import Splide from '@splidejs/splide';
-
 new Splide('.splide', {
+	type: 'loop',
+	perPage: 2,
 
-		type: 'loop',
-		perPage: 3,
-	});
+}).mount();
